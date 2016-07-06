d48a05bece40e30c38fe7f1d2ed46bb929c2daf8
David Campbell <david@mrcampbell.org>
2016-07-06 13:34:16 -0700

    Console::collect DRYs up #console
      10th question use case

--- lib/console/console.rb
+++ lib/console/console.rb
@@ -2,4 +2,9 @@ require 'colorize'
 require "readline"
 
 class Console
+  def self.collect(dir)
+    Dir[dir].sort_by{ |x| x[/\d+/].to_i }.each_with_index do |f, i|
+      yield(f, i)
+    end
+  end
 end
